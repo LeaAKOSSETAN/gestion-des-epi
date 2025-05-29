ea862f9ffbf942bbae8310aaa5463064b2b096c5
LeaAKOSSETAN <tayeakossetan@gmail.com>
2025-05-29 13:42:48 +0100

    rien juste pour le checkout

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -12,18 +12,17 @@ import ComptesGestion from './Components/admin/AdminDashbord';
 import AjouterUsers from './Components/admin/AjouterUsers';
 import GestionnaireDashbord from './Components/gestionnaire/GestionnaireDashbord';
 import ListeComptes from './Components/admin/ListeComptes';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import DqhseValidationDemandes from './Components/DQHSE/validation-demandes'
-import DqhseHistoriqueTransactions from './Components/DQHSE/historique-transactions'
-import DqhseDemandesEnAttente from './Components/DQHSE/demande-en-attente'
-import DqhseDashboard from './Components/DQHSE/dasboard-dqhse'
-import Login from './login'
-import Dashboard from './Components/employe/Dashboard'
-import DemandeEPI from './Components/employe/DemandeEpi'
-import Historique from './Components/employe/historique'
-import ListeDispo from './Components/employe/listedispo'
-import HistoriqueDemandes from './Components/employe/historiqueDemande'
+// import DqhseValidationDemandes from './Components/DQHSE/validation-demandes'
+// import DqhseHistoriqueTransactions from './Components/DQHSE/historique-transactions'
+// import DqhseDemandesEnAttente from './Components/DQHSE/demande-en-attente'
+// import DqhseDashboard from './Components/DQHSE/dasboard-dqhse'
+// import Login from './login'
+// import Dashboard from './Components/employe/Dashboard'
+// import DemandeEPI from './Components/employe/DemandeEpi'
+// import Historique from './Components/employe/historique'
+// import ListeDispo from './Components/employe/listedispo'
+// import HistoriqueDemandes from './Components/employe/historiqueDemande'
 
 function App() {
   return (
@@ -46,27 +45,27 @@ function App() {
     //   <StockPage/>
     // </div>
   );
-        {/* Routes pour les employés */}
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/demande-epi" element={<DemandeEPI />} />
-        <Route path="/historique" element={<Historique />} />
-        <Route path="/listeDispo" element={<ListeDispo />} />
-        <Route path="/historique-demandes" element={<HistoriqueDemandes />} />
+  //       {/* Routes pour les employés */}
+  //       <Route path="/" element={<Login />} />
+  //       <Route path="/dashboard" element={<Dashboard />} />
+  //       <Route path="/demande-epi" element={<DemandeEPI />} />
+  //       <Route path="/historique" element={<Historique />} />
+  //       <Route path="/listeDispo" element={<ListeDispo />} />
+  //       <Route path="/historique-demandes" element={<HistoriqueDemandes />} />
 
        
 
-                {/* Routes pour le dqhse de Département */}
+  //               {/* Routes pour le dqhse de Département */}
 
-                <Route path="/DQHSE/DQHSE-Dashboard" element={<DqhseDashboard/>} /> 
-      <Route path="/DQHSE/Validation-demande" element={<DqhseValidationDemandes/>} />
-      <Route path="/DQHSE/Transactions-plateforme" element={<DqhseHistoriqueTransactions />} />
-      <Route path="/DQHSE/Historique" element={<DqhseDemandesEnAttente />} />
-      {/* <Route path="/historique-demandes" element={<HistoriqueDemandes/>} />  */}
+  //               <Route path="/DQHSE/DQHSE-Dashboard" element={<DqhseDashboard/>} /> 
+  //     <Route path="/DQHSE/Validation-demande" element={<DqhseValidationDemandes/>} />
+  //     <Route path="/DQHSE/Transactions-plateforme" element={<DqhseHistoriqueTransactions />} />
+  //     <Route path="/DQHSE/Historique" element={<DqhseDemandesEnAttente />} />
+  //     {/* <Route path="/historique-demandes" element={<HistoriqueDemandes/>} />  */}
 
-      </Routes>
-    </Router>
-  )
+  //     </Routes>
+  //   </Router>
+  // )
 }
 
 export default App
--- front/src/routes/DQHSEroutes.tsx
+++ front/src/routes/DQHSEroutes.tsx
@@ -1,22 +1,22 @@
-import { Routes, Route } from 'react-router-dom'
-import DqhseValidationDemandes from '../Components/DQHSE/validation-demandes';
-import DqhseHistoriqueTransactions from '../Components/DQHSE/historique-transactions';
-import DqhseDemandesEnAttente from '../Components/DQHSE/demande-en-attente';
+// import { Routes, Route } from 'react-router-dom'
+// import DqhseValidationDemandes from '../Components/DQHSE/validation-demandes';
+// import DqhseHistoriqueTransactions from '../Components/DQHSE/historique-transactions';
+// import DqhseDemandesEnAttente from '../Components/DQHSE/demande-en-attente';
 
 
 
 
- function EmployeeRoutes() {
-  return (
-    <Routes>
-      {/* <Route path="/DQHSE/DQHSE-Dashboard" element={} /> */}
-      <Route path="/DQHSE/Validation-demande" element={<DqhseValidationDemandes/>} />
-      <Route path="/DQHSE/Transactions-plateforme" element={<DqhseHistoriqueTransactions />} />
-       <Route path="/DQHSE/Historique" element={<DqhseDemandesEnAttente />} />
-      {/* <Route path="/historique-demandes" element={<HistoriqueDemandes/>} />  */}
+//  function EmployeeRoutes() {
+//   return (
+//     <Routes>
+//       {/* <Route path="/DQHSE/DQHSE-Dashboard" element={} /> */}
+//       <Route path="/DQHSE/Validation-demande" element={<DqhseValidationDemandes/>} />
+//       <Route path="/DQHSE/Transactions-plateforme" element={<DqhseHistoriqueTransactions />} />
+//        <Route path="/DQHSE/Historique" element={<DqhseDemandesEnAttente />} />
+//       {/* <Route path="/historique-demandes" element={<HistoriqueDemandes/>} />  */}
 
 
-    </Routes>
-  )
-}
-export default EmployeeRoutes;
+//     </Routes>
+//   )
+// }
+// export default EmployeeRoutes;
